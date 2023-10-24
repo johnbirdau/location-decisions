05574d38e1477944a22b3e381ca223f532566547
John Bird <johnbirdau@gmail.com>
2023-10-24 12:01:31 +1100

    Add age grouping view

---
+++ back-end/AgeStructureDb/Migrations/20231024004714_PopulationByAgeGroup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgeStructureDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulationByAgeGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE OR ALTER VIEW dbo.PopulationByAgeGroup AS
+WITH
+RangeCte AS
+(
+	SELECT Value as RangeLow, Value + 4 As RangeHigh, CAST(Value as VARCHAR(5)) + '-' + CAST(Value+4 as VARCHAR(5)) AS RangeDisplay
+	FROM GENERATE_SERIES(0, (select MAX(CAST(Age as int)) + 4 from DimAges where ISNUMERIC(Age) = 1), 5)
+),
+SpecificAgeFactsCte AS
+(
+	SELECT AgeId, Value
+	FROM FactPopulations f
+	Where ISNUMERIC(f.AgeId) = 1
+)
+SELECT r.RangeLow, r.RangeHigh, r.RangeDisplay AS Range, SUM(f.Value) AS Value
+FROM RangeCte r
+LEFT JOIN SpecificAgeFactsCte f ON r.RangeLow <=  CAST(f.AgeId as int) AND r.RangeHigh >= CAST(f.AgeId as int)
+GROUP BY r.RangeLow, r.RangeHigh, r.RangeDisplay"
+
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW dbo.PopulationByAgeGroup;");
+        }
+    }
+}
